3b97fc1c332621732f818528155fddb4c5a8ad80
ame <shachi10969@gmail.com>
2022-07-21 19:41:26 +0900

    Initial Commit

---
+++ mapping/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mapping
+//
+//  Created by t&a on 2022/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mapping/mappingApp.swift
@@ -0,0 +1,17 @@
+//
+//  mappingApp.swift
+//  mapping
+//
+//  Created by t&a on 2022/07/21.
+//
+
+import SwiftUI
+
+@main
+struct mappingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
